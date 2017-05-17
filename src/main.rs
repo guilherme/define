7413826d60703158bbaeec2f41a57169ef03a9f8
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2017-05-17 19:57:48 +1000

    Extract class. Pearson Dictionary knows how to define its own words

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,30 @@ use std::env;
 use std::process;
 use std::io::Read;
 
-use serde_json::{Value, Error};
+use serde_json::{Value};
+
+
+
+struct PearsonDictionary {
+
+}
+impl PearsonDictionary {
+
+  fn define(term: &str) -> String {
+    let client = Client::new();
+    let mut res = client.get(format!("http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword={}", term).as_str()).send().unwrap();
+    let mut body = String::new();
+    match res.read_to_string(&mut body) {
+      Ok(_) => {
+        body
+      },
+      Err(_) => {
+        body
+      }
+    }
+  }
+}
+
 
 fn main() {
   let args: Vec<_> = env::args().collect();
@@ -18,29 +41,17 @@ fn main() {
     process::exit(1);
   }
 
-  let client = Client::new();
-  let mut res = client.get(format!("http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword={}", headword).as_str())
-    .send()
-    .unwrap();
-
-  let mut body = String::new();
-  match res.read_to_string(&mut body) {
-    Ok(_) => {
-      let v: Value = serde_json::from_str(body.as_str()).unwrap();
-      for words in v["results"].as_array().unwrap() {
-        println!("word: {}", words["headword"]);
-        for sense in words["senses"].as_array().unwrap() {
-          println!("definition(s):");
-          for definition in sense["definition"].as_array().unwrap() {
-            println!("{}", definition);
-          }
-        }
+  let body = PearsonDictionary::define(&headword);
+  let v: Value = serde_json::from_str(body.as_str()).unwrap();
+  for words in v["results"].as_array().unwrap() {
+    println!("word: {}", words["headword"]);
+    for sense in words["senses"].as_array().unwrap() {
+      println!("definition(s):");
+      for definition in sense["definition"].as_array().unwrap() {
+        println!("{}", definition);
       }
-      println!("{}", body);
-    },
-    Err(_)  => {
-      println!("error!");
     }
   }
+  println!("{}", body);
 
 }
